6dc469bd2739953dbaa897b896dfbcaa4c8c5d3f
mishra3452 <mishrasumit.3452@gmail.com>
2019-02-12 00:56:11 +0530

    nullandBool added

---
+++ 01BASICS/nullandBool.js
@@ -0,0 +1,27 @@
+//lets assume temp is coming from website API
+var temp = 0
+
+//done some calculation
+temp = 2
+
+//but what if temp is not having any value
+
+var temp2
+
+console.log("Current temperature is: " + temp)
+console.log(temp2) //undefined
+
+//Grade system
+
+// 10 - Great job
+// 5 - work harder
+// 2 - poor 
+// 0 - fail
+
+//boolean - True / false
+
+let actualMarks = 7
+
+let myGrade = (actualMarks <= 10)
+
+console.log(myGrade)
