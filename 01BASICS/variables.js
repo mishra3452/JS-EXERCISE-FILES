e16b1e7694cc82265d35e22f6af8b91fd9df8b95
mishra3452 <mishrasumit.3452@gmail.com>
2019-02-12 00:26:29 +0530

    Basic added

---
+++ 01BASICS/variables.js
@@ -0,0 +1,18 @@
+let name = 'sumit'
+
+let score = 103
+const bonus = 20
+
+let total = score + bonus
+
+console.log(total)
+
+console.log(name)
+
+let firstName = 'John'
+let lastName = 'Doe'
+
+console.log(firstName + ' ' + lastName)
+
+name = 'Sumit'
+console.log(name)
