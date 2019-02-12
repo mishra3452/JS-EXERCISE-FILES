a7f7b23c1d42158d42f3dadfafd3b95f91cc9dfc
mishra3452 <mishrasumit.3452@gmail.com>
2019-02-12 12:52:14 +0530

    marvels added

---
+++ 02Basics/marvels.js
@@ -0,0 +1,16 @@
+//datatype - Array
+
+//const superHeroes = ['Iron Man', 'Spiderman', 1, true]
+const superHeroes = ['Iron Man', 'Superman', 'Captain america']
+
+console.log(superHeroes)
+console.log(superHeroes[0])
+console.log(superHeroes[2])
+console.log(superHeroes[superHeroes.length -1]) 
+
+console.log('We have ' +  superHeroes.length + ' super heroes')
+
+console.log(`We have ${3+4} super heroes`)
+console.log(`We have ${superHeroes.length} super heroes`)
+
+console.log(`We have ${superHeroes[superHeroes.length -1]} super heroes`)
