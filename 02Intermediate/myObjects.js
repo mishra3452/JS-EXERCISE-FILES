dbaec0bef5a1b2c3d59cb9d9c72cc24d2b77a088
mishra3452 <mishrasumit.3452@gmail.com>
2019-02-12 18:01:05 +0530

    Objects added

---
+++ 02Intermediate/myObjects.js
@@ -0,0 +1,17 @@
+let myYoutubeVideo = {
+    title: 'Loops in javascript', 
+    videoLength: 15,
+    videoCreator: 'Sumit Mishra',
+    videoDescription: 'video description goes here'
+}
+
+console.log(myYoutubeVideo)
+
+console.log(`Hey new Video on ${myYoutubeVideo.title} by ${myYoutubeVideo.videoCreator}`)
+
+let courseDetails = {
+    title: 'Data Structures',
+    instructor: 'Sumit Mishra',
+    duration: '45min'
+} 
+console.log(courseDetails)
