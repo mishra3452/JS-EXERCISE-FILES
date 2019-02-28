a22d0fa362db1a228d6f91c54f83b9ae3a5c566f
mishra3452 <mishrasumit.3452@gmail.com>
2019-02-28 21:26:24 +0530

    web basics

---
+++ 01WebBasics/myscript.js
@@ -0,0 +1,10 @@
+// console.log(document.getElementById('idOne'))
+
+// console.log(document.getElementsByClassName('classOne'))
+
+const myElement = document.querySelectorAll('p')//this will give an array of all p
+console.log(myElement)
+console.log(myElement[0])//first element
+
+const myElementId = document.querySelectorAll('#idOne')
+console.log(myElementId)
